07d78a444f3325e776d984566f7dadaeb33d8236
superhuman <superhuman@localhost.localdomain>
2020-01-21 00:02:16 -0800

    scrolling text

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // function([string1, string2],target id,[color1,color2])    
-consoleText(['Hello World.', 'Console Text', 'Made with Love.'], 'text',['tomato','rebeccapurple','lightblue']);
+consoleText(['Hello Friend', 'Welcome To', 'My Portfollio'], 'text',['#33ff33','#33ff33','#33ff33']);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
